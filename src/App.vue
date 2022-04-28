832a0d689c388ec2d09c191ecf19cd67edaa3336
wangchao <wangxiaochaocccc@163.com>
2022-04-29 00:13:52 +0800

    feat(router): router的基本封装

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <p>222</p>
-  <hello-world>222</hello-world>
+ <router-view></router-view>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+export default {
+  name: 'app'
 }
+</script>
+
+<style>
+
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>home</p>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {}
+</script>
+<style></style>
