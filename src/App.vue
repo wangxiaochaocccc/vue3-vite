f407b79f9df99f861c22f232ada87588bd1a4ab5
wangchao <wangxiaochaocccc@163.com>
2022-04-28 21:02:38 +0800

    feat:eslint and prettier

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <p>222</p>
+  <hello-world>222</hello-world>
 </template>
 
 <style>
