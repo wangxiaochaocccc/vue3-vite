a46b0b0f8d710b644fb03cd17fc18563a6cf8196
wangchao <wangxiaochaocccc@163.com>
2022-04-28 22:19:13 +0800

    feat: test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
   <p>222</p>
-  <hello-world>222</hello-world>
+    <hello-world>222</hello-world>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
